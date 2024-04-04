e8342e036f38ae7dc73b8e9196b8d57bb9a77c1e
Iris Ruiz <146753865+Irillium@users.noreply.github.com>
2024-04-04 17:10:14 +0200

    Im bored T3T

---
+++ src/main/java/org/example/Id.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Id {
+    public String id;
+    public String name;
+    public String slug;
+    public Powerstats powerstats;
+    public Appearance appearance;
+    public Biography biography;
+    public Work work;
+    public Connections connections;
+    public Images images;
+}
